85181f5a35bf3187c21868ed3188af3be6841077
nicolas <nicolas@MacBook-Air-de-nicolas.local>
2026-06-26 12:06:20 -0400

    Conectar sistema a base de datos real de Firebase

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,15 +1,15 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY_AQUI",
+  apiKey: "AIzaSyBpOnza7soqLEqRCQ2bKKCd6U9oinw1O3Y",
   authDomain: "tavitajoyas.firebaseapp.com",
-  projectId: "tavitajoyas-....",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  projectId: "tavitajoyas",
+  storageBucket: "tavitajoyas.firebasestorage.app",
+  messagingSenderId: "623843810702",
+  appId: "1:623843810702:web:baaca8c95fcc846a2518df"
 };
 
-// ESTO ES LO QUE FALTA EN TU ARCHIVO:
-const app = initializeApp(firebaseConfig);
+// Inicializamos Firebase asegurándonos de que no se duplique
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const db = getFirestore(app);
